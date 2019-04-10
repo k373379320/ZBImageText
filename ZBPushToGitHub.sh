6e51cc992474a9b4d067b3ce81bbaa47fc6aa6e8
肖志斌 <zhibin.xiao@beibei.com>
2019-04-10 14:54:06 +0800

    feature:新增font,color容错,优化Color Blended Layers

--- ZBPushToGitHub.sh
+++ ZBPushToGitHub.sh
@@ -4,8 +4,8 @@ echo "pod update..."
 
 git add .
 git commit  -m "feature:新增font,color容错,优化Color Blended Layers"
-git tag "0.1.2"
+git tag "0.1.3"
 git push origin master
 git push --tags
-pod spec lint --verbose ZBImageText.podspec
-# pod trunk push ZBImageText.podspec --allow-warnings --verbose
+#pod spec lint --verbose ZBImageText.podspec --allow-warnings
+pod trunk push ZBImageText.podspec --allow-warnings --verbose
