c687e93da43bca5ca94ff9cee34e3fd686b6b563
肖志斌 <zhibin.xiao@beibei.com>
2019-04-26 18:08:41 +0800

    解决Nan问题

--- ZBPushToGitHub.sh
+++ ZBPushToGitHub.sh
@@ -4,8 +4,8 @@ echo "pod update..."
 
 git add .
 git commit  -m "解决Nan问题"
-git tag "0.1.5"
+git tag "0.1.6"
 git push origin master
 git push --tags
-pod spec lint --verbose ZBImageText.podspec --allow-warnings
-# pod trunk push ZBImageText.podspec --allow-warnings --verbose
+# pod spec lint --verbose ZBImageText.podspec --allow-warnings
+pod trunk push ZBImageText.podspec --allow-warnings --verbose
